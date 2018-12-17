b006ebefd42d97c766feb235364ad8cd83ddac54
Desi Mandasari <desimandasari1993@gmail.com>
2018-12-17 14:35:54 +0700

    move javacript code to its own file

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('Me: Hello World')
+console.log("World: (world's keep silent")
+console.log("Me: HELLO WORLD!!!!")
+console.log("World: (still silent)")
+console.log("Me: I SAID HELLO WOOORRLLLDDD!!!!")
+console.log("world: Earthquacke")
