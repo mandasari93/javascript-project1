f26af6d2876b0dfe5cfdb598a7feaea2a688a2a0
Desi Mandasari <desimandasari1993@gmail.com>
2018-12-19 10:36:59 +0700

    modified index.html, make new file exercise-2

---
+++ exercise-2/index.js
@@ -0,0 +1,20 @@
+const names = [
+  "Mr. Tatum",
+  "Mr. Evan",
+  "Mrs. Jolie",
+  "Mr. Pitt",
+  "Mr. Paul",
+  "Mrs. Paula",
+  "Mr. Hemsworth"
+];
+
+names.map(name => console.log("Meet with: " + name));
+
+const newNames = names.filter(newNames => newNames.includes("Mr."))
+console.log(newNames);
+
+const identify = names.map((name, i) => {
+    if (name.includes("Mr.")) console.log(`${i + 1}. ${name + " (male)"}`)
+    else if (name.includes("Mrs.")) console.log(`${i + 1}. ${name + " (female)"}`)
+}
+)
